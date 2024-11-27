f1bb2fc82f88d722b674f9c9caa28bd9fcfba8e1
zevtyardt <apklover76@gmail.com>
2024-11-27 11:26:32 +0700

    feat: implement FreeProxyListProvider for proxy fetching
    - Enhanced FreeProxyListProvider to fetch proxies from the Free Proxy List website.
    - Integrated HTML parsing using the scraper library to extract proxy IPs and ports from the table structure.
    - Implemented the sources method to provide the URL for fetching proxies.
    - Updated the scrape method to send parsed proxies through the channel and track the count using an atomic counter.

--- src/providers/free_proxy_list.rs
+++ src/providers/free_proxy_list.rs
@@ -1,23 +1,63 @@
-use std::sync::mpsc;
+use std::{
+    net::Ipv4Addr,
+    sync::{atomic::AtomicUsize, mpsc, Arc},
+};
 
 use async_trait::async_trait;
-use nipper::Document;
+use fake::{faker::internet::en::IPv4, Fake};
 use reqwest::{Client, Url};
+use scraper::{Html, Selector};
 
 use super::IProxyTrait;
-use crate::models::Proxy;
+use crate::models::{Proxy, Source};
 
-pub struct FreeProxyListProvider {}
+pub struct FreeProxyListProvider {
+    table: Selector,
+    row: Selector,
+    column: Selector,
+}
+
+impl Default for FreeProxyListProvider {
+    fn default() -> Self {
+        Self {
+            table: Selector::parse("table > tbody").unwrap(),
+            row: Selector::parse("tr").unwrap(),
+            column: Selector::parse("td").unwrap(),
+        }
+    }
+}
 
 #[async_trait]
 impl IProxyTrait for FreeProxyListProvider {
-    async fn fetch(&self, client: &Client) -> anyhow::Result<Document> {
-        Ok(Document::default())
+    fn sources(&self) -> Vec<Source> {
+        vec![Source::default("https://free-proxy-list.net/")]
+    }
+
+    async fn fetch(&self, client: &Client, url: Url) -> anyhow::Result<Html> {
+        let response = client.get(url).send().await?;
+        let text = response.text().await?;
+        Ok(Html::parse_document(&text))
     }
 
     async fn scrape(
-        &self, html: &Document, tx: mpsc::SyncSender<Proxy>,
-    ) -> anyhow::Result<Option<Url>> {
-        Ok(None)
+        &self, html: Html, tx: &mpsc::SyncSender<Proxy>, counter: &Arc<AtomicUsize>,
+    ) -> anyhow::Result<Vec<Source>> {
+        if let Some(table) = html.select(&self.table).next() {
+            for row in table.select(&self.row) {
+                let mut col = row.select(&self.column).map(|i| i.inner_html());
+                if let Some(Ok(ip)) = col.next().map(|f| f.parse::<Ipv4Addr>()) {
+                    if let Some(Ok(port)) = col.next().map(|f| f.parse::<u16>()) {
+                        let proxy = Proxy {
+                            ip,
+                            port,
+                            ..Default::default()
+                        };
+                        self.send(proxy, tx, counter);
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(vec![])
     }
 }
