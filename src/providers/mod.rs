9d88d0ef861f4ed9f9c3b55f2a80c55186c90367
zevtyardt <apklover76@gmail.com>
2024-11-27 11:25:54 +0700

    refactor: update IProxyTrait for improved proxy handling
    - Enhanced IProxyTrait to include methods for managing sources and asynchronous fetching of HTML.
    - Modified scrape method to accept parsed HTML and return a vector of sources.
    - Added a send method to handle sending proxies through the channel and updating the counter atomically.
    - Changed the HTML parsing library from nipper to scraper for better performance and capabilities.

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,17 +1,31 @@
-use std::sync::mpsc;
+use std::sync::{atomic::AtomicUsize, mpsc, Arc};
 
 use async_trait::async_trait;
-use nipper::Document;
 use reqwest::{Client, Url};
+use scraper::Html;
 
-use crate::models::{Anonymity, Proxy};
+use crate::models::{Anonymity, Proxy, Source};
 
 pub mod free_proxy_list;
 
 #[async_trait]
-trait IProxyTrait {
-    async fn fetch(&self, client: &Client) -> anyhow::Result<Document>;
+pub trait IProxyTrait {
+    fn sources(&self) -> Vec<Source>;
+
+    async fn fetch(&self, client: &Client, url: Url) -> anyhow::Result<Html>;
+
     async fn scrape(
-        &self, html: &Document, tx: mpsc::SyncSender<Proxy>,
-    ) -> anyhow::Result<Option<Url>>;
+        &self, html: Html, tx: &mpsc::SyncSender<Proxy>, counter: &Arc<AtomicUsize>,
+    ) -> anyhow::Result<Vec<Source>>;
+
+    fn send(
+        &self, proxy: Proxy, tx: &mpsc::SyncSender<Proxy>, counter: &Arc<AtomicUsize>,
+    ) {
+        if let Err(e) = tx.send(proxy) {
+            #[cfg(feature = "log")]
+            log::error!("Failed to send proxy, reason: {}", e);
+        } else {
+            counter.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+        }
+    }
 }
