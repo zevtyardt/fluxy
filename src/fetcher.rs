66214a7afc4decbea40e3d2136326c7f02bed493
zevtyardt <apklover76@gmail.com>
2024-11-27 11:25:06 +0700

    feat: enhance ProxyFetcher with concurrency and provider support
    - Refactored ProxyFetcher to support multiple providers and manage concurrent fetching of proxies.
    - Added user-agent randomization for requests and improved logging throughout the gathering process.
    - Introduced a counter for tracking the number of proxies gathered and modified the gathering logic to use a thread pool for efficiency.
    - Updated the default channel size for sender and receiver to improve performance during proxy collection.

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,28 +1,101 @@
-use std::{net::Ipv4Addr, sync::mpsc, time::Duration};
+use std::{
+    net::Ipv4Addr,
+    sync::{atomic::AtomicUsize, mpsc, Arc},
+    time::Duration,
+};
 
-use fake::{faker::internet::en::IPv4, Fake};
-use tokio::task::JoinHandle;
+use fake::{
+    faker::internet::en::{IPv4, UserAgent},
+    Fake,
+};
+use reqwest::ClientBuilder;
+use tokio::{task::JoinHandle, time};
+use tokio_task_pool::Pool;
 
-use crate::models::Proxy;
+use crate::{
+    models::Proxy,
+    providers::{free_proxy_list::FreeProxyListProvider, IProxyTrait},
+};
 
 pub struct ProxyFetcher {
     sender: mpsc::SyncSender<Proxy>,
     receiver: mpsc::Receiver<Proxy>,
-}
-
-impl ProxyFetcher {
-    pub async fn gather(&self) -> JoinHandle<()> {
-        #[cfg(feature = "log")]
-        log::debug!("");
-
-        tokio::spawn(async move {})
-    }
+    counter: Arc<AtomicUsize>,
+    providers: Vec<Arc<dyn IProxyTrait + Send + Sync>>,
 }
 
 impl Default for ProxyFetcher {
     fn default() -> Self {
-        let (sender, receiver) = mpsc::sync_channel(512);
-        Self { sender, receiver }
+        let (sender, receiver) = mpsc::sync_channel(1024);
+        Self {
+            sender,
+            receiver,
+            counter: Arc::new(AtomicUsize::new(0)),
+            providers: vec![],
+        }
+    }
+}
+
+impl ProxyFetcher {
+    pub fn use_default_providers(&mut self) {
+        self.providers = vec![Arc::new(FreeProxyListProvider::default())];
+    }
+
+    #[allow(unused_must_use)]
+    pub async fn gather(&self) -> anyhow::Result<JoinHandle<()>> {
+        #[cfg(feature = "log")]
+        log::debug!("Proxy gather started.");
+
+        let ua = UserAgent().fake::<&str>();
+        let client = ClientBuilder::new()
+            .timeout(Duration::from_secs(3))
+            .user_agent(ua)
+            .danger_accept_invalid_certs(true)
+            .danger_accept_invalid_hostnames(true)
+            .build()?;
+        #[cfg(feature = "log")]
+        log::debug!("Creating client with user-agent: {}", ua);
+
+        let counter = self.counter.clone();
+        counter.store(0, std::sync::atomic::Ordering::Relaxed);
+
+        let providers = self.providers.clone();
+        let sender = self.sender.clone();
+        let handle = tokio::spawn(async move {
+            let timer = time::Instant::now();
+            let pool = Pool::bounded(5);
+            for provider in providers.iter() {
+                let provider = Arc::clone(provider);
+                let client = client.clone();
+                let tx = sender.clone();
+                let counter = Arc::clone(&counter);
+                pool.spawn(async move {
+                    let mut sources = provider.sources();
+                    while let Some(source) = sources.pop() {
+                        if let Ok(html) = provider.fetch(&client, source.url).await {
+                            if let Ok(next_sources) =
+                                provider.scrape(html, &tx, &counter).await
+                            {
+                                sources.extend(next_sources);
+                            }
+                        }
+                    }
+                })
+                .await;
+            }
+
+            while pool.busy_permits().unwrap_or(0) != 0 {
+                time::sleep(Duration::from_millis(250)).await;
+            }
+            let total_proxies = counter.load(std::sync::atomic::Ordering::Acquire);
+            #[cfg(feature = "log")]
+            log::debug!(
+                "Proxy gather completed in {:?}. {} proxies where found. ",
+                timer.elapsed(),
+                total_proxies
+            );
+        });
+        Ok(handle)
     }
 }
 
@@ -30,7 +103,7 @@ impl Iterator for ProxyFetcher {
     type Item = Proxy;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Ok(proxy) = self.receiver.recv_timeout(Duration::from_secs(5)) {
+        if let Ok(proxy) = self.receiver.recv_timeout(Duration::from_millis(3000)) {
             Some(proxy)
         } else {
             None
