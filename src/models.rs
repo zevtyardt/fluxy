ead4eb961f50488e36cdc6f79ff8d75300869d36
zevtyardt <apklover76@gmail.com>
2024-11-27 11:24:03 +0700

    feat: enhance models with country and source structures
    - Added Country enum to represent proxy countries with display implementation.
    - Updated Proxy struct to include country information.
    - Introduced Source struct for managing URLs and default protocols for proxies.
    - Enhanced Display implementations for Proxy and Country for better logging and debugging.

--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,6 @@
-use std::net::Ipv4Addr;
+use std::{fmt::Display, net::Ipv4Addr, sync::Arc};
+
+use reqwest::Url;
 
 #[derive(Debug)]
 pub enum Anonymity {
@@ -14,12 +16,30 @@ pub enum Protocol {
     Https,
     Socks4,
     Socks5,
+    Connect25,
+    Connect80,
+}
+
+#[derive(Debug)]
+pub enum Country {
+    Unknown,
+    Id(String),
+}
+
+impl Display for Country {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Unknown => write!(f, "--"),
+            Self::Id(id) => write!(f, "{}", id),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Proxy {
     pub ip: Ipv4Addr,
     pub port: u16,
+    pub country: Country,
     pub anonymity: Anonymity,
     pub protocols: Vec<Protocol>,
 }
@@ -29,8 +49,36 @@ impl Default for Proxy {
         Self {
             ip: Ipv4Addr::new(0, 0, 0, 0),
             port: 0,
+            country: Country::Unknown,
             anonymity: Anonymity::Unknown,
             protocols: vec![],
+        }
+    }
+}
+
+impl Display for Proxy {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "<Proxy {} {}:{}>", self.country, self.ip, self.port)
+    }
+}
+
+pub struct Source {
+    pub url: Url,
+    pub default_protocols: Vec<Protocol>,
+}
+
+impl Source {
+    pub fn default(url: &str) -> Self {
+        Self {
+            url: Url::parse(url).unwrap(),
+            default_protocols: vec![
+                Protocol::Http,
+                Protocol::Https,
+                Protocol::Socks4,
+                Protocol::Socks5,
+                Protocol::Connect25,
+                Protocol::Connect80,
+            ],
         }
     }
 }
