4e7fcd18037b9a097c7fc3e1d10e8af79922b6b3
zevtyardt <apklover76@gmail.com>
2024-11-28 01:29:35 +0700

    refactor: improve IProxyTrait for better proxy management
    - Updated the fetch method to accept a string URL instead of Url type for flexibility.
    - Modified the scrape method to include default_protocols, enhancing scraper customization.
    - Refined the send method to return a boolean indicating the success of sending proxies, improving error handling.

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -4,7 +4,7 @@ use async_trait::async_trait;
 use reqwest::{Client, Url};
 use scraper::Html;
 
-use crate::models::{Anonymity, Proxy, Source};
+use crate::models::{Anonymity, Protocol, Proxy, Source};
 
 pub mod free_proxy_list;
 
@@ -12,22 +12,21 @@ pub mod free_proxy_list;
 pub trait IProxyTrait {
     fn sources(&self) -> Vec<Source>;
 
-    async fn fetch(&self, client: &Client, url: Url) -> anyhow::Result<Html>;
+    async fn fetch(&self, client: &Client, url: &str) -> anyhow::Result<Html>;
 
     async fn scrape(
         &self, html: Html, tx: &mpsc::SyncSender<Option<Proxy>>,
-        counter: &Arc<AtomicUsize>,
+        counter: &Arc<AtomicUsize>, default_protocols: Vec<Arc<Protocol>>,
     ) -> anyhow::Result<Vec<Source>>;
 
     fn send(
         &self, proxy: Proxy, tx: &mpsc::SyncSender<Option<Proxy>>,
         counter: &Arc<AtomicUsize>,
-    ) {
-        if let Err(e) = tx.send(Some(proxy)) {
-            #[cfg(feature = "log")]
-            log::error!("Failed to send proxy, reason: {}", e);
-        } else {
-            counter.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+    ) -> bool {
+        if tx.send(Some(proxy)).is_ok() {
+            counter.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            return true;
         }
+        false
     }
 }
