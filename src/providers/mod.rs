d3a9075a11fc6eb566b0f2615ae529abc6973873
zevtyardt <apklover76@gmail.com>
2024-11-30 01:01:51 +0700

    docs: add documentation comments to providers module
    - Added comments to clarify the purpose and functionality of the IProxyTrait and its methods.
    - Improved the overall documentation for the providers module, enhancing clarity on the proxy fetching process.

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,29 +1,37 @@
 use std::sync::{atomic::AtomicUsize, mpsc, Arc};
 
 use async_trait::async_trait;
-use reqwest::{Client, Url};
+use reqwest::Client;
 use scraper::Html;
 
-use crate::models::{Anonymity, Protocol, Proxy, Source};
+use crate::models::{Protocol, Proxy, Source};
 
+/// Module for fetching proxies from free proxy lists.
 pub mod free_proxy_list;
+
+/// Module for fetching proxies from GitHub repositories.
 pub mod github;
 
+/// Trait defining the behavior of proxy providers.
 #[async_trait]
 pub trait IProxyTrait {
+    /// Returns a list of sources from which proxies can be fetched.
     fn sources(&self) -> Vec<Source>;
 
+    /// Fetches the HTML content from the specified URL using the provided HTTP client.
     async fn fetch(&self, client: Client, url: &str) -> anyhow::Result<Html> {
         let response = client.get(url).send().await?;
         let text = response.text().await?;
         Ok(Html::parse_document(&text))
     }
 
+    /// Scrapes proxy information from the fetched HTML content.
     async fn scrape(
         &self, html: Html, tx: mpsc::Sender<Option<Proxy>>, counter: Arc<AtomicUsize>,
         default_types: Vec<Arc<Protocol>>,
     ) -> anyhow::Result<()>;
 
+    /// Sends a found proxy through the provided channel and updates the counter.
     fn send(
         &self, proxy: Proxy, tx: &mpsc::Sender<Option<Proxy>>, counter: &Arc<AtomicUsize>,
     ) -> bool {
