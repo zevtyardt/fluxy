9ed2c37ff6f88d65589c8a453321342ef3358b88
zevtyardt <apklover76@gmail.com>
2024-11-29 09:23:13 +0700

    chore(main): initialize ProxyFetcher with options
    - Updated main function to create ProxyFetcher using ProxyFetcherOptions.
    - Adjusted proxy fetching logic to take a single proxy instead of two for output.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use fluxy::fetcher::ProxyFetcher;
+use fluxy::fetcher::{ProxyFetcher, ProxyFetcherOptions};
 #[cfg(feature = "log")]
 use fluxy::setup_log;
 
@@ -7,11 +7,12 @@ async fn main() -> anyhow::Result<()> {
     #[cfg(feature = "log")]
     setup_log(log::LevelFilter::Debug)?;
 
-    let mut f = ProxyFetcher::default();
+    let opts = ProxyFetcherOptions::default();
+    let mut f = ProxyFetcher::new(opts);
     f.use_default_providers();
     f.gather().await?;
 
-    for p in f.filter(|p| p.port == 80).take(2) {
+    for p in f.take(1) {
         println!("{:#?}", p);
     }
 
