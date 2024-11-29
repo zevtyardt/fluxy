fc8ba285b50de8fe0d269cc6c79516d68aabaf19
zevtyardt <apklover76@gmail.com>
2024-11-30 01:00:47 +0700

    refactor(main): switch to manual Tokio runtime management
    - Replaced the Tokio async main macro with a manual runtime management approach.
    - Improved structure by creating a multi-threaded Tokio runtime and executing async operations within it.
    - Updated output to count and display the number of proxies retrieved instead of printing a single proxy.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use fluxy::fetcher::{ProxyFetcher, ProxyFetcherOptions};
 #[cfg(feature = "log")]
 use fluxy::setup_log;
+use tokio::runtime;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     #[cfg(feature = "log")]
     setup_log(log::LevelFilter::Debug)?;
 
-    let opts = ProxyFetcherOptions::default();
-    let mut f = ProxyFetcher::new(opts).await?;
-    f.use_default_providers();
-    f.gather().await?;
+    let runtime = runtime::Builder::new_multi_thread().enable_all().build()?;
 
-    println!("{:#?}", f.get_one());
+    runtime.block_on(async {
+        let opts = ProxyFetcherOptions::default();
+        let mut f = ProxyFetcher::new(opts).await?;
+        f.use_default_providers();
+        f.gather().await?;
 
-    Ok(())
+        println!("{:#?}", f.iter().take(56).count());
+        Ok(())
+    })
 }
