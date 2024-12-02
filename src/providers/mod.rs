c47b8993662d69336f35c393ec30b544054d495a
zevtyardt <apklover76@gmail.com>
2024-12-02 19:06:08 +0700

    refactor: update IProxyTrait fetch method to use hyper
    - Replaced reqwest client with hyper for fetching HTML content in the IProxyTrait trait.
    - Implemented a more robust fetch method that handles redirects and uses a timeout for requests.
    - Utilized User-Agent header for requests to mimic browser behavior.
    - Adapted to work with hyper`s request/response mechanisms, ensuring better performance and compatibility.

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,8 +1,17 @@
-use std::sync::{atomic::AtomicUsize, mpsc, Arc};
+use std::{
+    collections::VecDeque,
+    sync::{atomic::AtomicUsize, mpsc, Arc},
+    time::Duration,
+};
 
 use async_trait::async_trait;
-use reqwest::Client;
+use fake::{faker::internet::en::UserAgent, Fake};
+use http_body_util::{BodyExt, Empty};
+use hyper::{body::Bytes, Request};
+use hyper_tls::HttpsConnector;
+use hyper_util::client::legacy::{connect::HttpConnector, Client};
 use scraper::Html;
+use tokio::time;
 
 use crate::models::{Protocol, Proxy, Source};
 
@@ -18,11 +27,42 @@ pub trait IProxyTrait {
     /// Returns a list of sources from which proxies can be fetched.
     fn sources(&self) -> Vec<Source>;
 
-    /// Fetches the HTML content from the specified URL using the provided HTTP client.
-    async fn fetch(&self, client: Client, url: &str) -> anyhow::Result<Html> {
-        let response = client.get(url).send().await?;
-        let text = response.text().await?;
-        Ok(Html::parse_document(&text))
+    /// Fetches the HTML content from the specified URL.
+    async fn fetch(
+        &self, client: Arc<Client<HttpsConnector<HttpConnector>, Empty<Bytes>>>,
+        url: &str, timeout: Duration,
+    ) -> anyhow::Result<Html> {
+        let mut urls = VecDeque::new();
+        urls.push_back((url.to_string(), None));
+
+        let ua = UserAgent().fake::<&str>();
+
+        let mut content = String::new();
+        while let Some((url, previous_url)) = urls.pop_front() {
+            let mut req = Request::builder()
+                .uri(&url)
+                .header(hyper::header::USER_AGENT, ua);
+            if let Some(previous_url) = previous_url {
+                req = req.header(hyper::header::REFERER, previous_url);
+            }
+
+            let mut response =
+                time::timeout(timeout, client.request(req.body(Empty::<Bytes>::new())?))
+                    .await??;
+            if let Some(redirect) = response.headers().get(hyper::header::LOCATION) {
+                let redirect = redirect.to_str()?;
+                urls.push_back((redirect.to_string(), Some(url)));
+                continue;
+            }
+            while let Some(next) = response.frame().await {
+                let frame = next?;
+                if let Some(chunk) = frame.data_ref() {
+                    let chunk_str = String::from_utf8_lossy(chunk);
+                    content.push_str(&chunk_str);
+                }
+            }
+        }
+        Ok(Html::parse_document(&content))
     }
 
     /// Scrapes proxy information from the fetched HTML content.
