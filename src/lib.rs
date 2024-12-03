67ee71c4125a3ceb1baea355c4ae72689eb4da7b
zevtyardt <apklover76@gmail.com>
2024-12-03 08:46:00 +0700

    refactor(lib): enhance documentation and structure in lib.rs
    - Improved module organization by rearranging the order of module declarations.
    - Added detailed documentation for the setup_log function, including arguments and return values.
    - Clarified comments within the logging setup for better understanding of its functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
-/// Module responsible for fetching proxies from various sources.
-pub mod fetcher;
-
-/// Module that handles geographical IP lookups and data.
-pub mod geoip;
-
-/// Module defining the data models used throughout the application.
-pub mod models;
-
-/// Module containing different proxy providers.
-pub mod providers;
-
-/// Low level custom client based on hyper to perform simple http requests.
 pub mod client;
+pub mod fetcher;
+pub mod geoip;
+pub mod models;
+pub mod providers;
+pub mod validator;
 
+/// Sets up logging for the application.
+///
+/// This function initializes the logging system with the specified log level.
+///
+/// # Arguments
+///
+/// * `log_level`: The level of logging verbosity. It determines what log messages will be shown.
+///
+/// # Returns
+///
+/// A result indicating success or failure during the logging setup.
 #[cfg(feature = "log")]
-/// Sets up logging for the application with the specified log level.
 pub fn setup_log(log_level: log::LevelFilter) -> anyhow::Result<()> {
     #[cfg(feature = "log")]
     stderrlog::new()
-        .module(module_path!())
-        .show_module_names(true)
-        .verbosity(log_level)
-        .init()?;
+        .module(module_path!()) // Sets the module path for log messages.
+        .show_module_names(true) // Enables displaying module names in log output.
+        .verbosity(log_level) // Sets the specified log level.
+        .init()?; // Initializes the logger.
     Ok(())
 }
