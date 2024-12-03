753acd7ace4ef417130f17781634e04fc317b11f
zevtyardt <apklover76@gmail.com>
2024-12-03 08:50:17 +0700

    refactor(main): improve response handling and logging in main function
    - Enhanced response handling to display status and headers of the HTTP response.
    - Updated logging to provide clearer output for the fetched proxies.
    - Cleaned up the flow of the main function by removing unnecessary comments and code.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::time::Duration;
 
+use fake::{faker::internet::en::UserAgent, Fake};
 #[cfg(feature = "log")]
 use fluxy::setup_log;
 use fluxy::{
     client::ProxyClient,
     fetcher::ProxyFetcher,
-    models::{Protocol, ProxyFetcherConfig, ProxyFilter},
+    models::{Protocol, ProxyConfig},
+    validator::ProxyValidator,
 };
-use http_body_util::Empty;
+use http_body_util::{BodyExt, Empty};
 use hyper::{body::Bytes, Request};
 use tokio::runtime;
 
@@ -21,29 +23,37 @@ fn main() -> anyhow::Result<()> {
         .build()?;
 
     runtime.block_on(async {
-        let config = ProxyFetcherConfig {
-            filters: ProxyFilter {
-                types: vec![Protocol::Http(fluxy::models::Anonymity::Unknown)],
+        let config = ProxyConfig {
+            filters: fluxy::models::ProxyFilter {
+                types: vec![Protocol::Https],
                 ..Default::default()
             },
             ..Default::default()
         };
 
-        let mut f = ProxyFetcher::new(config).await?;
-        f.gather().await?;
+        let mut fetcher = ProxyFetcher::gather(config).await?;
+        let mut validator = ProxyValidator::from(fetcher.iter());
 
-        for proxy in f.iter() {
+        for proxy in validator.iter().take(500) {
             let mut client = ProxyClient::new(proxy, Duration::from_secs(5));
             let req = Request::builder()
-                .uri("https://google.com")
-                .header(hyper::header::HOST, "google.com")
+                .uri("https://httpbin.org/get?show_env")
+                .header(hyper::header::USER_AGENT, UserAgent().fake::<&str>())
                 .body(Empty::<Bytes>::new())?;
-            if let Err(e) = client.send_request(req).await {
-                client.log_error(e);
-                continue;
+            match client.send_request(req).await {
+                Ok(res) => {
+                    println!("Response: {}", res.status());
+                    println!("Headers: {:#?}\n", res.headers());
+                    let c = res.into_body().collect().await?;
+                    println!("Body: {}", String::from_utf8_lossy(&c.to_bytes()));
+	            println!("{}", client.proxy);
+
+                    break;
+                }
+                Err(e) => {
+                    client.log_error(e);
+                }
             }
-            println!("{}", client.proxy);
-            break;
         }
         Ok(())
     })
