dfe17132c2588fb0b1f3d429c99a67f146625bac
zevtyardt <apklover76@gmail.com>
2024-12-03 08:47:44 +0700

    feat(validator): add ProxyValidator for managing proxy iteration
    - Introduced ProxyValidator to encapsulate proxy iteration logic.
    - Implemented ProxyValidatorIter to facilitate the iteration over Proxy instances.
    - Enhanced code structure for better management of proxy validation and iteration.

---
+++ src/validator.rs
@@ -0,0 +1,27 @@
+use crate::models::Proxy;
+
+pub struct ProxyValidator<I: Iterator> {
+    iterable: I,
+}
+
+impl<I: Iterator<Item = Proxy>> ProxyValidator<I> {
+    pub fn from(iterable: I) -> Self {
+        Self { iterable }
+    }
+
+    pub fn iter(&mut self) -> ProxyValidatorIter<I> {
+        ProxyValidatorIter { inner: self }
+    }
+}
+
+pub struct ProxyValidatorIter<'a, I: Iterator> {
+    inner: &'a mut ProxyValidator<I>,
+}
+
+impl<I: Iterator<Item = Proxy>> Iterator for ProxyValidatorIter<'_, I> {
+    type Item = Proxy;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.iterable.next()
+    }
+}
