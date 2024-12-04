5f1f3bbad44c9a0ea391c8fd433110f9d0909b35
zevtyardt <apklover76@gmail.com>
2024-12-04 14:19:28 +0700

    feat(main): enhance main function for proxy requests
    - Improved request handling with timeout and HTTPS negotiation
    - Updated request to use myip.wtf for IP visibility
    - Enhanced response logging, including status, headers, and body content
    - Adjusted ProxyConfig for increased concurrency and changed default proxy types

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,92 @@
-use std::time::Duration;
+use std::{sync::Arc, time::Duration};
 
-use fake::{faker::internet::en::UserAgent, Fake};
 #[cfg(feature = "log")]
 use fluxy::setup_log;
 use fluxy::{
     client::ProxyClient,
     fetcher::ProxyFetcher,
     models::{Protocol, ProxyConfig},
+    negotiators::https::HttpsNegotiator,
     validator::ProxyValidator,
 };
-use http_body_util::{BodyExt, Empty};
+use http_body_util::{BodyExt, Full};
 use hyper::{body::Bytes, Request};
-use tokio::runtime;
+use hyper_tls::HttpsConnector;
+use hyper_util::{client::legacy::Client, rt::TokioExecutor};
+use tokio::{runtime, time::timeout};
 
 fn main() -> anyhow::Result<()> {
     #[cfg(feature = "log")]
     setup_log(log::LevelFilter::Trace)?;
 
-    let runtime = runtime::Builder::new_multi_thread()
-        .worker_threads(8)
-        .enable_all()
-        .build()?;
+    let runtime = runtime::Builder::new_multi_thread().enable_all().build()?;
 
     runtime.block_on(async {
         let config = ProxyConfig {
             filters: fluxy::models::ProxyFilter {
-                types: vec![Protocol::Https],
+                types: vec![Protocol::Http(fluxy::models::Anonymity::Unknown)],
                 ..Default::default()
             },
+            concurrency_limit: 10,
             ..Default::default()
         };
 
         let mut fetcher = ProxyFetcher::gather(config).await?;
         let mut validator = ProxyValidator::from(fetcher.iter());
 
-        for proxy in validator.iter().take(500) {
-            let mut client = ProxyClient::new(proxy, Duration::from_secs(5));
+        for proxy in validator.iter() {
+            let mut client = ProxyClient::new(proxy);
             let req = Request::builder()
-                .uri("https://httpbin.org/get?show_env")
-                .header(hyper::header::USER_AGENT, UserAgent().fake::<&str>())
-                .body(Empty::<Bytes>::new())?;
-            match client.send_request(req).await {
-                Ok(res) => {
-                    println!("Response: {}", res.status());
-                    println!("Headers: {:#?}\n", res.headers());
+                .uri("https://myip.wtf/text")
+                .header(hyper::header::HOST, "myip.wtf")
+                .body(Full::<Bytes>::from(""))?;
+            match timeout(
+                Duration::from_secs(3),
+                client.send_request(req.clone(), Arc::new(HttpsNegotiator)),
+            )
+            .await
+            {
+                Ok(Ok(res)) => {
+                    let status = res.status();
+                    println!("Response: {}", status);
+                    println!("Headers: {:#?}", res.headers());
                     let c = res.into_body().collect().await?;
-                    println!("Body: {}", String::from_utf8_lossy(&c.to_bytes()));
-	            println!("{}", client.proxy);
+                    let d = c.to_bytes();
+                    let v = serde_json::from_slice::<serde_json::Value>(&d.clone())
+                        .map(|v| {
+                            serde_json::to_string_pretty(&v).unwrap_or("Empty".into())
+                        })
+                        .unwrap_or(String::from_utf8_lossy(&d).to_string());
+                    println!("Body: {}", v);
+                    println!("{}", client.proxy);
+                    if status.is_success() {
+                        let https = HttpsConnector::new();
+                        let c = Client::builder(TokioExecutor::new()).build(https);
+                        let res2 = c.request(req).await?;
+                        println!("Response: {}", res2.status());
+                        println!("Headers: {:#?}", res2.headers());
+                        let c = res2.into_body().collect().await?;
+                        let d = c.to_bytes();
+                        let v = serde_json::from_slice::<serde_json::Value>(&d.clone())
+                            .map(|v| {
+                                serde_json::to_string_pretty(&v).unwrap_or("Empty".into())
+                            })
+                            .unwrap_or(String::from_utf8_lossy(&d).to_string());
+                        println!("Body: {}", v);
 
-                    break;
+                        break;
+                    }
                 }
+                Ok(Err(e)) => {
+                    client.log_error(e);
+                }
+
                 Err(e) => {
                     client.log_error(e);
                 }
             }
         }
+
         Ok(())
     })
 }
