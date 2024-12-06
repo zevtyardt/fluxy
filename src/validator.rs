7b11e7808afd2dbefbbc3339577871db72f72921
zevtyardt <apklover76@gmail.com>
2024-12-06 07:35:22 +0700

    refactor(validator): enhance proxy validation structure and logic
    - Simplified ProxyValidator to operate without a generic iterator, using crossbeam_channel for communication
    - Updated validation logic to filter proxies based on provided types
    - Added error handling for empty protocol types and unsupported protocols
    - Improved drop implementation to handle task abortion gracefully

--- src/validator.rs
+++ src/validator.rs
@@ -1,30 +1,73 @@
+use std::time::Duration;
 
-use crate::models::Proxy;
+use tokio::{task::JoinHandle, time};
+use tokio_task_pool::Pool;
 
-pub struct ProxyValidator<I: Iterator> {
-    // sender: mpsc::SyncSender<I::Item>,
-    // receiver: mpsc::Receiver<I::Item>,
-    iterable: I,
+use crate::models::{Anonymity, Protocol, Proxy, ProxyValidatorConfig};
+
+pub struct ProxyValidator {
+    receiver: crossbeam_channel::Receiver<Option<Proxy>>,
+    handler: JoinHandle<()>,
 }
 
-impl<I: Iterator<Item = Proxy>> ProxyValidator<I> {
-    pub fn from(iterable: I) -> Self {
-        Self { iterable }
+impl ProxyValidator {
+    pub async fn validate<I>(proxy_source: I, config: ProxyValidatorConfig) -> anyhow::Result<Self>
+    where
+        I: Iterator<Item = Proxy> + Send + 'static,
+    {
+        if config.types.is_empty() {
+            anyhow::bail!("config.types cannot be empty")
+        }
+
+        if config.types.contains(&Protocol::Http(Anonymity::Unknown)) {
+            anyhow::bail!("can't use Protocol::Http(Anonymity::Unknown) for proxy filtering")
+        }
+
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        let handler = tokio::spawn(async move {
+            let pool = Pool::bounded(config.concurrency_limit);
+            for mut proxy in proxy_source {
+                proxy.types.retain(|type_| {
+                    let left_proto = &*type_.protocol;
+                    config
+                        .types
+                        .iter()
+                        .any(|right_proto| match (left_proto, right_proto) {
+                            (Protocol::Http(_), Protocol::Http(_))
+                            | (Protocol::Connect(_), Protocol::Connect(_)) => true,
+                            _ => left_proto == right_proto,
+                        })
+                });
+                if proxy.types.is_empty() {
+                    continue;
+                }
+
+                let _ = sender.send(Some(proxy));
+            }
+
+            // Wait for all tasks in the pool to complete.
+            while pool.busy_permits().unwrap_or(0) != 0 {
+                time::sleep(Duration::from_millis(50)).await;
+            }
+            sender.send(None).unwrap_or_default();
+        });
+        Ok(Self { receiver, handler })
     }
+}
 
-    pub fn iter(&mut self) -> ProxyValidatorIter<I> {
-        ProxyValidatorIter { inner: self }
+impl Drop for ProxyValidator {
+    fn drop(&mut self) {
+        self.handler.abort();
     }
 }
 
-pub struct ProxyValidatorIter<'a, I: Iterator> {
-    inner: &'a mut ProxyValidator<I>,
-}
-
-impl<I: Iterator<Item = Proxy>> Iterator for ProxyValidatorIter<'_, I> {
+impl Iterator for ProxyValidator {
     type Item = Proxy;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.iterable.next()
+        if let Ok(proxy) = self.receiver.recv() {
+            return proxy;
+        }
+        None
     }
 }
