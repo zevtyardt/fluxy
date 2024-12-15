4649cb2b671988c2806992d39f8e38b9ba768db4
zevtyardt <apklover76@gmail.com>
2024-12-15 22:48:52 +0700

    refactor(cli): enhance argument parsing and application flow
    - Simplified CLI argument structure by removing subcommands for a more straightforward interface
    - Enhanced proxy validation and fetching logic based on user-defined types and countries
    - Improved error reporting for invalid type values
    - Refactored output handling for better JSON and text formatting

--- src/cli/argument.rs
+++ src/cli/argument.rs
@@ -1,6 +1,6 @@
 use clap::builder::styling::AnsiColor;
 use clap::builder::{PossibleValue, Styles};
-use clap::{Args, Parser, Subcommand};
+use clap::Parser;
 
 fn get_styles() -> Styles {
     Styles::styled()
@@ -17,153 +17,74 @@ fn get_styles() -> Styles {
     styles=get_styles()
 )]
 pub struct Cli {
+    /// List of ISO country codes to filter proxies by location.
+    #[arg(short, long, num_args(1..))]
+    pub countries: Vec<String>,
+
     /// Maximum number of concurrent proxy checks.
-    #[arg(long, default_value = "50")]
+    #[arg(short, long, default_value = "500")]
     pub max_connections: usize,
 
     /// Timeout duration in seconds before giving up.
-    #[arg(short, long, default_value = "8")]
-    pub timeout: usize,
+    #[arg(long, default_value = "3")]
+    pub timeout: u64,
 
     /// Log level for application output.
     #[arg(
         long = "log",
-        default_value = "warn",
+        default_value = "off",
         value_parser([
             PossibleValue::new("debug"),
             PossibleValue::new("info"),
             PossibleValue::new("warn"),
             PossibleValue::new("error"),
+            PossibleValue::new("trace"),
+            PossibleValue::new("off"),
         ])
     )]
     pub log_level: String,
 
-    /// Subcommands available for the proxy application.
-    #[command(subcommand)]
-    pub command: Commands,
-}
-
-/// Subcommands for the proxy application.
-#[derive(Subcommand, Debug, Clone)]
-#[command(
-    after_help = "Suggestions and bug reports are greatly appreciated:\nhttps://github.com/zevtyardt/fluxy/issues",
-    styles=get_styles()
-)]
-pub enum Commands {
-    /// Retrieve proxies without performing any checks.
-    Fetch(FetchArgs),
-
-    /// Retrieve and validate proxies.
-    Find(FindArgs),
-}
-
-/// Arguments for the `grab` subcommand.
-#[derive(Args, Debug, Clone)]
-#[command(
-    after_help = "Suggestions and bug reports are greatly appreciated:\nhttps://github.com/zevtyardt/fluxy/issues",
-    styles=get_styles()
-)]
-pub struct FetchArgs {
-    /// List of ISO country codes to filter proxies by location.
-    #[arg(short, long, num_args(1..))]
-    pub countries: Vec<String>,
+    /// Output format for the results.
+    #[arg(
+        short,
+        long,
+        default_value = "default",
+        value_parser([
+            PossibleValue::new("default"),
+            PossibleValue::new("text"),
+            PossibleValue::new("json"),
+        ])
+    )]
+    pub format: String,
 
     /// Maximum number of proxies to retrieve.
     #[arg(short, long, default_value = "0")]
     pub limit: usize,
 
-    /// Output format for the results.
-    #[arg(
-        short,
-        long,
-        default_value = "default",
-        value_parser([
-            PossibleValue::new("default"),
-            PossibleValue::new("text"),
-            PossibleValue::new("json"),
-        ])
-    )]
-    pub format: String,
-
     /// File path to save the retrieved proxies. If not provided, output will go to the console.
     #[arg(short, long)]
     pub output_file: Option<std::path::PathBuf>,
-}
 
-/// Arguments for the `find` subcommand.
-#[derive(Args, Debug, Clone)]
-#[command(
-    after_help = "Suggestions and bug reports are greatly appreciated:\nhttps://github.com/zevtyardt/fluxy/issues",
-    styles=get_styles()
-
-)]
-pub struct FindArgs {
-    /// Proxy types (protocols) to validate.
+    /// Proxy types (protocols) to validate. [possible values: HTTP{:Transparent,
+    /// :Anonymous,:Elite}, HTTPS, SOCKS4, SOCKS5, CONNECT:<port>]
     #[arg(
-        long,
-        required = true,
+        short = 't',
+        long = "types",
+        help_heading = "Validate",
         num_args(1..),
-        value_parser([
-            PossibleValue::new("HTTP"),
-            PossibleValue::new("HTTPS"),
-            PossibleValue::new("SOCKS4"),
-            PossibleValue::new("SOCKS5"),
-            PossibleValue::new("CONNECT:80"),
-            PossibleValue::new("CONNECT:25"),
-        ])
     )]
-    pub proxy_types: Vec<String>,
+    pub types: Vec<String>,
 
-    /// File paths containing proxies. Overrides providers if specified.
-    #[arg(long, num_args(1..))]
-    pub proxy_files: Vec<std::path::PathBuf>,
-
-    /// Levels of anonymity for HTTP proxies. Defaults to any level.
-    #[arg(
-        long,
-        num_args(1..),
-        value_parser([
-            PossibleValue::new("Transparent"),
-            PossibleValue::new("Anonymous"),
-            PossibleValue::new("High"),
-        ])
-    )]
-    pub anonymity_levels: Vec<String>,
+    /// File path containing proxies. Overrides providers if specified.
+    #[arg(long, help_heading = "Validate", requires("types"))]
+    pub file: Option<std::path::PathBuf>,
 
     /// Maximum number of attempts to validate a proxy.
-    #[arg(long, default_value = "1")]
-    pub max_attempts: usize,
-
-    /// Require the proxy to support cookies.
-    #[arg(long, default_value = "false")]
-    pub supports_cookies: bool,
-
-    /// Require the proxy to support referer headers.
-    #[arg(long, default_value = "false")]
-    pub supports_referer: bool,
-
-    /// List of ISO country codes to filter proxies by location.
-    #[arg(short, long, num_args(1..))]
-    pub countries: Vec<String>,
-
-    /// Maximum number of working proxies to retrieve.
-    #[arg(short, long, default_value = "0")]
-    pub limit: usize,
-
-    /// Output format for the results.
     #[arg(
-        short,
         long,
-        default_value = "default",
-        value_parser([
-            PossibleValue::new("default"),
-            PossibleValue::new("text"),
-            PossibleValue::new("json"),
-        ])
+        default_value = "1",
+        help_heading = "Validate",
+        requires("types")
     )]
-    pub format: String,
-
-    /// File path to save the validated proxies. If not provided, output will go to the console.
-    #[arg(short, long)]
-    pub output_file: Option<std::path::PathBuf>,
+    pub max_attempts: usize,
 }
--- src/cli/main.rs
+++ src/cli/main.rs
@@ -1,31 +1,175 @@
+use std::{fs::File, io::Write};
+
+use argument::Cli;
+use clap::{
+    error::{ContextKind, ContextValue, ErrorKind},
+    CommandFactory, Parser,
+};
 #[cfg(feature = "log")]
 use fluxy::initialize_logging;
-use fluxy::{proxy::models::Protocol, ProxySource, ProxyValidator};
+use fluxy::{
+    proxy::models::{Anonymity, Protocol, Proxy},
+    ProxySource, ProxyValidator,
+};
 use tokio::runtime;
 
 mod argument;
 
-fn main() -> anyhow::Result<()> {
+fn main() {
+    if let Err(e) = run_application() {
+        eprintln!("Error: {:?}", e);
+    }
+}
+
+#[allow(unused_must_use)]
+fn report_invalid_type_value(value: &str) {
+    let mut error = clap::Error::new(ErrorKind::ValueValidation).with_cmd(&Cli::command());
+    error.insert(
+        ContextKind::InvalidArg,
+        ContextValue::String("--types".to_owned()),
+    );
+    error.insert(
+        ContextKind::InvalidValue,
+        ContextValue::String(value.to_string()),
+    );
+    error.print();
+}
+
+fn run_application() -> anyhow::Result<()> {
+    let options = Cli::parse();
+
     #[cfg(feature = "log")]
-    initialize_logging(log::LevelFilter::Off)?;
+    {
+        let log_level = match options.log_level.as_str() {
+            "debug" => log::LevelFilter::Debug,
+            "info" => log::LevelFilter::Info,
+            "warn" => log::LevelFilter::Warn,
+            "error" => log::LevelFilter::Error,
+            "trace" => log::LevelFilter::Trace,
+            _ => log::LevelFilter::Off,
+        };
+        initialize_logging(log_level)?;
+    }
 
     let runtime = runtime::Builder::new_multi_thread().enable_all().build()?;
     runtime.block_on(async {
-        let proxy_source = ProxySource::from_fetcher(fluxy::fetcher::Config::default()).await?;
-        let validated_proxy = ProxyValidator::validate(
-            proxy_source,
-            fluxy::validator::Config {
-                types: vec![
-                    Protocol::Https,
-                    Protocol::Http(fluxy::proxy::models::Anonymity::Elite),
-                ],
-                ..Default::default()
-            },
-        )
-        .await?;
+        let proxy_source: Box<dyn Iterator<Item = Proxy> + Send + 'static> =
+            if let Some(file) = options.file {
+                Box::new(ProxySource::from_file(file)?)
+            } else {
+                Box::new(
+                    ProxySource::from_fetcher(fluxy::fetcher::Config {
+                        request_timeout: options.timeout,
+                        concurrency_limit: 5,
+                        countries: options.countries,
+                        ..Default::default()
+                    })
+                    .await?,
+                )
+            };
 
-        for proxy in validated_proxy {
-            //            println!("{}", proxy.as_json()?);
+        let mut output_file = options.output_file.map(|file_path| {
+            File::options()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(file_path)
+                .unwrap()
+        });
+
+        let proxy_iterator: Box<dyn Iterator<Item = Proxy>> = if !options.types.is_empty() {
+            let protocols = options
+                .types
+                .iter()
+                .map_while(|type_str| {
+                    let mut parts = type_str.split(':');
+                    if let Some(protocol) = parts.next() {
+                        match protocol {
+                            "HTTP" => {
+                                if let Some(anonymity) = parts.next() {
+                                    match anonymity {
+                                        "Transparent" => {
+                                            return Some(Protocol::Http(Anonymity::Transparent))
+                                        }
+                                        "Anonymous" => {
+                                            return Some(Protocol::Http(Anonymity::Anonymous))
+                                        }
+                                        "Elite" => return Some(Protocol::Http(Anonymity::Elite)),
+                                        _ => {}
+                                    }
+                                }
+                                return Some(Protocol::Http(Anonymity::Unknown));
+                            }
+                            "HTTPS" => return Some(Protocol::Https),
+                            "SOCKS4" => return Some(Protocol::Socks4),
+                            "SOCKS5" => return Some(Protocol::Socks5),
+                            "CONNECT" => {
+                                if let Some(Ok(port)) = parts.next().map(|p| p.parse::<u16>()) {
+                                    return Some(Protocol::Connect(port));
+                                }
+                            }
+                            _ => report_invalid_type_value(type_str),
+                        }
+                    }
+                    None
+                })
+                .collect::<Vec<_>>();
+
+            if protocols.is_empty() {
+                std::process::exit(-1)
+            }
+            let validated_proxies = ProxyValidator::validate(
+                proxy_source,
+                fluxy::validator::Config {
+                    types: protocols,
+                    concurrency_limit: options.max_connections,
+                    max_attempts: options.max_attempts,
+                    request_timeout: options.timeout,
+                },
+            )
+            .await?;
+
+            Box::new(validated_proxies)
+        } else {
+            Box::new(proxy_source)
+        };
+
+        for (index, proxy) in proxy_iterator.enumerate() {
+            let should_end = options.limit > 0 && index + 1 >= options.limit;
+            let output = match options.format.as_str() {
+                "text" => proxy.as_text(),
+                "json" => {
+                    let mut json_output = String::new();
+                    if index == 0 {
+                        json_output.push_str("[\n");
+                    }
+                    json_output.push_str("  ");
+                    json_output.push_str(&proxy.as_json());
+                    if !should_end {
+                        json_output.push(',');
+                    }
+                    json_output
+                }
+                _ => format!("{}", proxy),
+            };
+
+            if let Some(ref mut file) = output_file {
+                file.write_all(output.as_bytes())?;
+                file.write_all(b"\n")?;
+            } else {
+                println!("{}", output);
+            }
+
+            if should_end {
+                break;
+            }
+        }
+        if options.format == "json" {
+            if let Some(ref mut file) = output_file {
+                file.write_all(b"]")?;
+            } else {
+                println!("]");
+            }
         }
         Ok(())
     })
