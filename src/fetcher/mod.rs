bf4e05fcb35c3723770504e7de3ecc34abbe9694
zevtyardt <apklover76@gmail.com>
2024-12-19 09:56:56 +0700

    refactor(fetcher): optimize ProxyFetcher structure and logic
    - Replaced crossbeam_channel with kanal for asynchronous proxy handling
    - Simplified the proxy gathering process and improved concurrency management
    - Enhanced unique IP tracking and overall code organization for better readability

--- src/fetcher/mod.rs
+++ src/fetcher/mod.rs
@@ -1,16 +1,9 @@
 mod config;
 
-use std::{
-    collections::HashSet,
-    net::Ipv4Addr,
-    sync::{
-        atomic::{AtomicUsize, Ordering},
-        Arc,
-    },
-    time::Duration,
-};
+use std::{borrow::Cow, sync::Arc, time::Duration};
 
 pub use config::Config;
+use hashbrown::HashSet;
 use http_body_util::Empty;
 use hyper::body::Bytes;
 use hyper_tls::HttpsConnector;
@@ -18,8 +11,7 @@ use hyper_util::{
     client::legacy::{connect::HttpConnector, Client},
     rt::TokioExecutor,
 };
-use tokio::{task::JoinHandle, time};
-use tokio_task_pool::Pool;
+use tokio::{sync::Semaphore, task::JoinHandle, time};
 
 use crate::{
     geolookup::GeoLookup,
@@ -31,16 +23,16 @@ use crate::{
 
 /// Responsible for fetching proxies from various sources.
 pub struct ProxyFetcher {
-    sender: crossbeam_channel::Sender<Option<Proxy>>, // Channel sender for passing proxies.
-    receiver: crossbeam_channel::Receiver<Option<Proxy>>, // Channel receiver for receiving proxies.
-    counter: Arc<AtomicUsize>, // Counter for tracking the number of fetched proxies.
-    timer: time::Instant,      // Timer for measuring elapsed time.
-    elapsed: Option<Duration>, // Duration of the fetcher operation.
-    geolookup: Option<GeoLookup>, // Optional GeoIP instance for location lookups.
+    sender: kanal::AsyncSender<Proxy>, // Channel sender for passing proxies.
+    receiver: kanal::Receiver<Proxy>,  // Channel receiver for receiving proxies.
+    counter: usize,                    // Counter for tracking the number of fetched proxies.
+    timer: time::Instant,              // Timer for measuring elapsed time.
+    elapsed: Option<Duration>,         // Duration of the fetcher operation.
+    geolookup: Option<GeoLookup>,      // Optional GeoIP instance for location lookups.
     providers: Vec<Arc<dyn IProxyTrait + Send + Sync>>, // List of proxy providers.
-    unique_ips: HashSet<(Ipv4Addr, u16)>, // Set to track unique IPs.
-    handler: Option<JoinHandle<()>>, // Handle for the fetching task.
-    config: Config,            // Configuration for the proxy fetcher.
+    unique_ips: HashSet<Cow<'static, str>>, // Set to track unique IPs.
+    handlers: Vec<JoinHandle<()>>,     // Handle for the fetching task.
+    config: Config,                    // Configuration for the proxy fetcher.
 }
 
 impl ProxyFetcher {
@@ -54,7 +46,7 @@ impl ProxyFetcher {
     ///
     /// A result containing the initialized `ProxyFetcher`.
     pub async fn gather(config: Config) -> anyhow::Result<Self> {
-        let (sender, receiver) = crossbeam_channel::unbounded();
+        let (sender, receiver) = kanal::unbounded();
         let geolookup = if config.enable_geo_lookup {
             Some(GeoLookup::new().await?)
         } else {
@@ -62,21 +54,21 @@ impl ProxyFetcher {
         };
 
         let providers: Vec<Arc<dyn IProxyTrait + Send + Sync>> = vec![
-            Arc::new(FreeProxyListProvider::default()),
-            Arc::new(GithubRepoProvider),
+            //Arc::new(GithubRepoProvider),
             Arc::new(ProxyscrapeProvider),
+            //Arc::new(FreeProxyListProvider),
         ];
 
         let mut fetcher = Self {
-            sender,
-            receiver,
-            counter: Arc::new(AtomicUsize::new(0)),
+            sender: sender.to_async(),
+            counter: 0,
             timer: time::Instant::now(),
             elapsed: None,
-            geolookup,
-            handler: None,
-            providers,
+            handlers: vec![],
             unique_ips: HashSet::new(),
+            geolookup,
+            providers,
+            receiver,
             config,
         };
 
@@ -90,8 +82,7 @@ async fn do_work(
     provider: Arc<dyn IProxyTrait + Send + Sync>,
     client: Arc<Client<HttpsConnector<HttpConnector>, Empty<Bytes>>>,
     source: Arc<Source>,
-    tx: crossbeam_channel::Sender<Option<Proxy>>,
-    counter: Arc<AtomicUsize>,
+    tx: kanal::AsyncSender<Proxy>,
 ) -> anyhow::Result<()> {
     let html = provider
         .fetch(client, &source.url.to_string(), source.timeout)
@@ -106,7 +97,7 @@ async fn do_work(
             checked: false,
         })
         .collect();
-    provider.scrape(html, tx, counter, proxy_types).await
+    provider.scrape(html, tx, proxy_types).await
 }
 
 impl ProxyFetcher {
@@ -122,12 +113,6 @@ impl ProxyFetcher {
     /// Gathers proxies from the configured providers.
     #[allow(unused_must_use)]
     async fn start(&mut self) -> anyhow::Result<()> {
-        // Abort any ongoing gathering process if it exists.
-        if let Some(handler) = &self.handler {
-            handler.abort();
-            self.handler = None;
-        }
-
         let providers = self.providers.clone();
         let mut tasks = vec![];
         for provider in providers.iter() {
@@ -142,40 +127,27 @@ impl ProxyFetcher {
             tasks.len(),
         );
 
-        let counter = self.counter.clone();
-        counter.store(0, Ordering::Relaxed);
-        let sender = self.sender.clone();
-
         let client = Arc::new(
             Client::builder(TokioExecutor::new()).build::<_, Empty<Bytes>>(HttpsConnector::new()),
         );
         let concurrency_limit = self.config.concurrency_limit;
+        let sem = Arc::new(Semaphore::new(concurrency_limit));
 
-        let handler = tokio::spawn(async move {
-            let pool = Pool::bounded(concurrency_limit);
-            for (source, provider) in tasks.iter() {
-                let source = Arc::clone(source);
-                let provider = Arc::clone(provider);
-                let client = Arc::clone(&client);
-                let tx = sender.clone();
-                let counter = Arc::clone(&counter);
-                pool.spawn(async move {
+        for (source, provider) in tasks {
+            let permit = Arc::clone(&sem);
+            let client = Arc::clone(&client);
+            let tx = self.sender.clone();
+
+            self.handlers.push(tokio::spawn(async move {
+                if permit.acquire().await.is_ok() {
                     let url = source.url.to_string();
-                    if let Err(e) = do_work(provider, client, source, tx, counter).await {
+                    if let Err(e) = do_work(provider, client, source, tx).await {
                         #[cfg(feature = "log")]
                         log::error!("{}: {}", url, e);
                     }
-                })
-                .await;
-            }
-
-            // Wait for all tasks in the pool to complete.
-            while pool.busy_permits().unwrap_or(0) != 0 {
-                // do nothing
-            }
-            sender.send(None).unwrap_or_default();
-        });
-        self.handler = Some(handler);
+                }
+            }));
+        }
         Ok(())
     }
 
@@ -187,32 +159,34 @@ impl ProxyFetcher {
     ///
     /// An optional `Proxy` if one is available, otherwise `None`.
     pub fn get_one(&mut self) -> Option<Proxy> {
-        while let Some(mut proxy) = self.receiver.recv().ok()? {
-            if let Some(geolookup) = &self.geolookup {
-                proxy.geo = geolookup.lookup(&proxy.ip);
+        while !self.receiver.is_empty() || self.receiver.sender_count() - 1 != 0 {
+            if let Ok(mut proxy) = self.receiver.recv_timeout(Duration::from_millis(100)) {
+                if let Some(geolookup) = &self.geolookup {
+                    proxy.geo = geolookup.lookup(&proxy.ip);
 
-                if !self.config.countries.is_empty()
-                    && !proxy
-                        .geo
-                        .iso_code
-                        .clone()
-                        .map(|code| self.config.countries.contains(&code))
-                        .unwrap_or(false)
-                {
-                    self.counter.fetch_sub(1, Ordering::Relaxed);
-                    continue;
+                    if !self.config.countries.is_empty()
+                        && !proxy
+                            .geo
+                            .iso_code
+                            .clone()
+                            .map(|code| self.config.countries.contains(&code))
+                            .unwrap_or(false)
+                    {
+                        continue;
+                    }
                 }
-            }
 
-            if self.config.enforce_unique_ip {
-                if self.unique_ips.insert((proxy.ip, proxy.port)) {
-                    return Some(proxy);
+                if self.config.enforce_unique_ip {
+                    if self.unique_ips.insert(proxy.as_text()) {
+                        self.counter += 1;
+                        return Some(proxy);
+                    } else {
+                        continue;
+                    }
                 } else {
-                    self.counter.fetch_sub(1, Ordering::Relaxed);
-                    continue;
+                    self.counter += 1;
+                    return Some(proxy);
                 }
-            } else {
-                return Some(proxy);
             }
         }
         None
@@ -222,35 +196,25 @@ impl ProxyFetcher {
 impl Iterator for ProxyFetcher {
     type Item = Proxy;
 
-    /// Retrieves the next proxy from the fetcher.
-    ///
-    /// # Returns
-    ///
-    /// An optional `Proxy` if available, otherwise `None`.
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(proxy) = self.get_one() {
-            return Some(proxy);
-        }
-        self.elapsed = Some(self.timer.elapsed());
-        None
+        self.get_one()
     }
 }
 
 impl Drop for ProxyFetcher {
     /// Cleans up resources when `ProxyFetcher` is dropped.
     fn drop(&mut self) {
-        self.sender.send(None).unwrap_or_default();
-        if let Some(handler) = &self.handler {
+        self.receiver.close();
+        while let Some(handler) = self.handlers.pop() {
             handler.abort();
         }
 
         #[cfg(feature = "log")]
-        let total_proxies = self.counter.load(Ordering::Acquire);
         #[cfg(feature = "log")]
         log::debug!(
             "Proxy gathering completed in {:?}. {} proxies were found",
             self.elapsed.unwrap_or(self.timer.elapsed()),
-            total_proxies
+            self.counter
         );
     }
 }
